093d476bf38845422beb0e31aafb89470058b399
Kacper <120328307+kacper-the-programmer@users.noreply.github.com>
2024-12-03 22:07:49 +0100

    created grades.cpp and changed fibonacci.hpp

--- fibonacci.hpp
+++ fibonacci.hpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 void fib(int n, int num = 0, int next_num = 1) {
 	if (n > 0) { 
 		std::cout << num << ", "; 
---
+++ grades.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <locale>
+
+const int students = 19;
+int grades[students];
+
+float average_grade = 0.0;
+std::wstring good_grades, bad_grades;
+
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_CTYPE, "Polish");
+
+	for (int i = 0; i < students; i++)
+	{
+		grades[i] = 1 + rand() % 6;
+		std::wcout << i + 1 << ": " << grades[i] << ", \n";
+	}
+
+	for (int i = 0; i < students; i++)
+	{
+		average_grade += grades[i];
+		if (grades[i] >= 5) {
+			good_grades += std::to_wstring(i + 1) + L", ";
+		}
+		if (grades[i] == 1) {
+			bad_grades += std::to_wstring(i + 1) + L", ";
+		}
+	}
+	std::wcout << L"\noceny powyżej piątki: " << good_grades;
+	std::wcout << L"\noceny równe jeden: " << bad_grades;
+
+	std::wcout << L"\n\nśrednia ocen: " << (float)(average_grade / students) << std::endl;
+}
